80a6af0021667c5460abdae65841860572ee416a
Bob <bobrecio@BobsMBP.local>
2021-12-31 10:35:02 -0600

    restored project on macbook

---
+++ src/pages/Checkout.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+    <title>Razors: Checkout</title>
+</svelte:head>
+<h1>Checkout Page</h1>
---
+++ src/pages/Login.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Razors: Login</title>
+</svelte:head>
+
+<h1>Login Page</h1>
