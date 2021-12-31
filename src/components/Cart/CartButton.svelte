baeefb83d4af63cd3ff07c63410b7a2647ba43e8
Bob <bobrecio@BobsMBP.local>
2021-12-31 15:55:36 -0600

    finished razors/ch285 - cart totals

--- src/components/Cart/CartButton.svelte
+++ src/components/Cart/CartButton.svelte
@@ -1,6 +1,9 @@
 <script>
   import globalStore from "../../stores/globalStore.js";
-
+  import cart from "../../stores/cart.js";
+$: total = $cart.reduce((accum,curr)=>{
+  return (accum += curr.amount)
+},0);
 </script>
 <!-- cart toggle -->
 <div class="btn-cart-container">
@@ -12,5 +15,5 @@
   >
     <i class="fas fa-cart-plus" />
   </button>
-  <span class="btn-cart-items">10</span>
+  <span class="btn-cart-items">{total}</span>
 </div>
