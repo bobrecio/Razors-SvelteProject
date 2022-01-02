c3f7d36f82fb89a65266bbd5765946e092ab55bc
bobrecio <bobrecio@gmail.com>
2022-01-02 17:32:57 -0600

    finsihed razors/ch308

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,5 +1,58 @@
-<svelte:head>
-    <title>Razors: Login</title>
-</svelte:head>
+<script>
+  let email = "";
+  let password = "";
+  let username = "default username";
+  let isMember = true;
+  $: isEmpty = !name || !password || !username;
 
-<h1>Login Page</h1>
+  function toggleMember() {}
+
+  async function handleSubmit() {}
+</script>
+
+<section class="form">
+  <h2 class="section-title">
+    {#if isMember}sign in{:else}register{/if}
+  </h2>
+  <form class="login-form" on:submit|preventDefault={handleSubmit}>
+    <!-- single input -->
+    <div class="form-control">
+      <label for="email" />
+      <input type="email" id="email" bind:value={email} />
+    </div>
+    <!-- end of single input -->
+    <!-- single input -->
+    <div class="form-control">
+      <label for="password" />
+      <input type="password" id="password" bind:value={password} />
+    </div>
+    <!-- end of single input -->
+    {#if !isMember}
+      <!-- single input -->
+      <div class="form-control">
+        <label for="username" />
+        <input type="username" id="username" bind:value={username} />
+      </div>
+      <!-- end of single input -->
+    {/if}
+    {#if isEmpty}
+      <p class="form-empty">Please fill all fields</p>
+      <button
+        class="btn btn-block btn-primary"
+        type="button"
+        disabled={isEmpty}
+        class:disabled={isEmpty}>submit</button
+      >
+      <p class="register-link">
+        Need to register?<button type="button" on:click={toggleMember()}
+          >click here</button
+        >
+      </p>
+    {:else}
+      <p class="register-link">
+        already a member?
+        <button type="button" on:click={toggleMember}>click here</button>
+      </p>
+    {/if}
+  </form>
+</section>
--- src/stores/defaultProduct.js
+++ src/stores/defaultProduct.js
@@ -1,17 +1,30 @@
 import {writable,derived} from 'svelte/store';
-import localProducts from '../localProducts';
+//import localProducts from '../localProducts';
+import url from '../strapi/URL';
+import getProducts from '../strapi/getProducts';
 
-const store = writable(flattenProducts([...localProducts])); // normal operations
-//const store = writable([]); // testing with nothing loading
+const store = writable([], () => {
+    setProducts();
+    return () => {};
+}); // normal operations
+
+async function setProducts() {
+    let products = await getProducts();
+    if (products){
+        products = flattenProducts(products);
+        store.set(products);
+    }
+}
 
 // flatten products
 function flattenProducts(data){
     return data.map(item =>{
-        let imageUrl = item.image.url;
-        return {...item, image:imageUrl};
+        // let imageUrl = item.image.url;
+        let image = `${url}${item.image.url}`
+        return {...item, image};
     })
 }
-// feautured stores
+// featured stores
 export const featuredStore = derived(store, $featured => {
     // console.log($featured);
     return $featured.filter((item) => item.featured === true);
