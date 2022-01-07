6ccc08952af376d6ab1e4e45b70278e1c65888c7
bobrecio <bobrecio@gmail.com>
2022-01-06 21:03:40 -0600

    developing on xps13 again

---
+++ src/components/Navbar/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import SmallNavbar from "./SmallNavbar.svelte";
+  import BigNavbar from "./BigNavbar.svelte";
+  import globalStore from "../../stores/globalStore";
+  let screenWidth;
+  // we don't need the sidebar if it's a big screen
+  $: if (screenWidth > 992) {
+    globalStore.toggleItem("sidebar", false);
+  }
+</script>
+
+<svelte:window bind:innerWidth={screenWidth} />
+
+{#if screenWidth > 992}
+  <BigNavbar />
+{:else}
+  <SmallNavbar />
+{/if}
